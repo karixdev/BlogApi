95bf81eaf825378c6b518a1614c2a4635a02a0c0
karixdev <kari.klucz@gmail.com>
2022-11-05 21:43:22 +0100

    feat: create User entity

---
+++ src/main/java/com/github/karixdev/blogapi/user/User.java
@@ -0,0 +1,135 @@
+package com.github.karixdev.blogapi.user;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity(name = "User")
+@Table(
+        name = "user",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "user_email_unique",
+                        columnNames = "email"
+                )
+        }
+)
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(
+            name = "id",
+            nullable = false,
+            updatable = false
+    )
+    private UUID id;
+
+    @Column(
+            name = "email",
+            nullable = false
+    )
+    private String email;
+
+    @Column(
+            name = "first_name",
+            nullable = false
+    )
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(
+            name = "password",
+            nullable = false
+    )
+    private String password;
+
+    @Column(
+            name = "user_role",
+            nullable = false
+    )
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    @Column(
+            name = "is_enabled",
+            nullable = false
+    )
+    private Boolean isEnabled;
+
+    public User(
+            String email,
+            String firstName,
+            String lastName,
+            String password,
+            UserRole userRole,
+            Boolean isEnabled
+    ) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.userRole = userRole;
+        this.isEnabled = isEnabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority =
+                new SimpleGrantedAuthority(userRole.name());
+
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(password, user.password) && userRole == user.userRole && Objects.equals(isEnabled, user.isEnabled);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, firstName, lastName, password, userRole, isEnabled);
+    }
+}
---
+++ src/main/java/com/github/karixdev/blogapi/user/UserRole.java
@@ -0,0 +1,6 @@
+package com.github.karixdev.blogapi.user;
+
+public enum UserRole {
+    USER,
+    ADMIN
+}
