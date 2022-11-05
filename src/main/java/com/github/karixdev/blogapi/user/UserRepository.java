650632329e4ae7c3e7524f8d731d3658e6542a43
karixdev <kari.klucz@gmail.com>
2022-11-05 22:38:57 +0100

    fix: add repository annotation to UserRepository

--- src/main/java/com/github/karixdev/blogapi/user/UserRepository.java
+++ src/main/java/com/github/karixdev/blogapi/user/UserRepository.java
@@ -2,11 +2,13 @@ package com.github.karixdev.blogapi.user;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Transactional(readOnly = true)
+@Repository
 public interface UserRepository {
     @Query("""
             SELECT user
