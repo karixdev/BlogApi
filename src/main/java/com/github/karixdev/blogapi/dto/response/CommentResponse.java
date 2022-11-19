859f44b42824a967d7e9db5fe8cc7ef3dc487958
karixdev <kari.klucz@gmail.com>
2022-11-19 16:27:53 +0100

    feat: add votes count to CommentResponse

--- src/main/java/com/github/karixdev/blogapi/dto/response/CommentResponse.java
+++ src/main/java/com/github/karixdev/blogapi/dto/response/CommentResponse.java
@@ -1,7 +1,9 @@
 package com.github.karixdev.blogapi.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.karixdev.blogapi.entity.Comment;
+import com.github.karixdev.blogapi.entity.CommentVote;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,9 +21,13 @@ public class CommentResponse {
     @JsonIgnoreProperties({"email", "is_enabled", "role"})
     private UserResponse author;
 
+    @JsonProperty("votes")
+    private VoteCountResponse<CommentVote> voteCountResponse;
+
     public CommentResponse(Comment comment) {
         this.id = comment.getId();
         this.content = comment.getContent();
         this.author = new UserResponse(comment.getAuthor());
+        this.voteCountResponse = new VoteCountResponse<>(comment.getVotes());
     }
 }
