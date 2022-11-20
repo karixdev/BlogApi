12da503828f2769c1ea1c624e2197f8d3f1db2d1
karixdev <kari.klucz@gmail.com>
2022-11-20 15:06:44 +0100

    refactor: extract sending email to separate method

--- src/main/java/com/github/karixdev/blogapi/service/EmailService.java
+++ src/main/java/com/github/karixdev/blogapi/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.github.karixdev.blogapi.service;
 
 import com.github.karixdev.blogapi.entity.ConfirmationToken;
+import com.github.karixdev.blogapi.entity.User;
 import lombok.AllArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -16,22 +17,26 @@ public class EmailService {
 
     private final JavaMailSender sender;
 
-    @Async
     public void sendEmailConfirmation(ConfirmationToken token) {
+        send(token.getUser(), "Confirm your email",
+                getEmailConfirmationTemplate(token));
+    }
+
+    @Async
+    public void send(User user, String topic, String body) {
         MimeMessage mimeMessage = sender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
 
         try {
-            helper.setText(getEmailConfirmationTemplate(token), true);
-            helper.setTo(token.getUser().getEmail());
-            helper.setSubject("Confirm your email");
+            helper.setText(body, true);
+            helper.setTo(user.getEmail());
+            helper.setSubject(topic);
             helper.setFrom("no-reply@blogapi.com");
 
             sender.send(mimeMessage);
         } catch (MessagingException e) {
             throw new IllegalStateException("Failed to send an email confirmation");
         }
-
     }
 
     private String getEmailConfirmationTemplate(ConfirmationToken token) {
